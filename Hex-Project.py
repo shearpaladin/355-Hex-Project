376f5ca8b4b358f6f93f5330f066b5cc56b14425
shearpaladin <shearpaladin@gmail.com>
2020-11-21 16:56:17 -0700

    3x3 Board Inserted

--- Hex-Project.py
+++ Hex-Project.py
@@ -2,25 +2,28 @@
 import pygame, sys
 from pygame.locals import *
 
-# GLOBALS
-# Define Grid Size
-grid = [ [1] * 8 for n in range(8)]
+## GLOBALS ##
 
 # Store Hexagons
 hexagon_list = []
-# define shape sizes
-w = 70
+
+
+
 # define colors
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 GRAY = (220,220,220)
 
+
+
 # Vertices
-INITIAL_HEXAGON_VERTICES = ((-40,-40),(40,-40),(45,0),(40,40),(-40,40),(-45,0))
+#INITIAL_HEXAGON_VERTICES = ((-40,-40),(40,-40),(45,0),(40,40),(-40,40),(-45,0))
+#INITIAL_HEXAGON_VERTICES = ((-20,-20),(20,-20),(25,0),(20,20),(-20,20),(-25,0))
+INITIAL_HEXAGON_VERTICES = ((50,25),(100,0),(150,25),(150,75),(100,100),(50,75))
 
 # Size of Grid
-GRID_HEIGHT = 10
-GRID_WIDTH = 10
+GRID_HEIGHT = 3
+GRID_WIDTH = 3
 
 # Each Vertice in the hexagon
 VERTEX_COUNT = 6
@@ -42,26 +45,40 @@ pygame.init()
 windowSurface = pygame.display.set_mode((800, 600),0, 32)
 
 # Set Caption of Project
-pygame.display.set_caption('Hex Project')
+pygame.display.set_caption('Hex Project 3x3')
 
 # Background Color
 #windowSurface.fill(WHITE)
 
 
 index = 0
+
 for column in range(GRID_WIDTH):
     for row in range(GRID_HEIGHT):
 
         points = []
-        lift_hexagon = 0
+        #lift_hexagon = 0
 
-        if column % 2 != 0:
-            lift_hexagon = 40
+        lift_hexagon_x = 0
+        lift_hexagon_y = 0
+
+        # if column % 2 != 0:
+        #     lift_hexagon_x = 5
+
+        if row % 2 != 0:
+            lift_hexagon_x = -50
+            lift_hexagon_y = 25
+
+        elif row % 3 != 0:
+            lift_hexagon_x = -100
+            lift_hexagon_y = 50
+
+
 
         for point in range(VERTEX_COUNT):
 
-            points.append(  ((INITIAL_HEXAGON_VERTICES[point][0] + (85 * column)),
-                            ((INITIAL_HEXAGON_VERTICES[point][1] + (80 * row))-lift_hexagon)  ) )
+            points.append(  ( (INITIAL_HEXAGON_VERTICES[point][0] + (100 * column) -lift_hexagon_x),
+                            ( (INITIAL_HEXAGON_VERTICES[point][1] + (100 * row))-lift_hexagon_y)  ) )
 
         new_hexagon = Hexagon(index,points)
         hexagon_list.append(new_hexagon)
@@ -74,7 +91,6 @@ for i in range(len(hexagon_list)):
     pygame.draw.polygon(windowSurface,BLACK,hexagon_list[i].vertices,3)
 
 pygame.display.flip()
-
 
 
 
