1df6c8eb10f380a144f93db855a70726398bcea6
hellosusan <ngocthao@ualberta.ca>
2020-11-24 02:29:05 -0500

    fix simulation

--- HexGame/HexBot.py
+++ HexGame/HexBot.py
@@ -26,7 +26,6 @@ class Node:
             exploration = float(sqrt(2 * log(self.parent.num_simuls) / self.num_simuls))
             self.value = exploitation + exploration
 
-
 class HexBot:
     def __init__(self, play_color, state):
         self.play_color = play_color
@@ -71,10 +70,10 @@ class HexBot:
 
             print('Count: ' + str(count))
 
-            current_node, current_state = self.selection_expansion(player) # selection + expansion
+            current_node, current_state, player = self.selection_expansion(player) # selection + expansion
 
             ++count
-            outcome = self.simulation(current_state, player)
+            outcome, player = self.simulation(current_state, player)
             self.back_propagation(current_node, outcome)
 
     def selection_expansion(self, player):
@@ -83,32 +82,32 @@ class HexBot:
 
         while len(current_node.children) > 0:
             current_node = self.find_max_node(current_node)
-            self.play(current_state, player, current_node.move)
 
             if current_node.value == NEG_INFINITY:
-                return current_node, current_state
+                return current_node, current_state, 3 - player
 
         if game_status(current_node.state) == UNKNOWN:  # expansion
             available_moves = find_empty_cells(current_node.state)
 
             for move in available_moves:  # add children
                 # define child
-                new_state = copy.deepcopy(current_node.state)
-                self.play(new_state, player, move)
+                new_state, player = self.play(copy.deepcopy(current_node.state), player, move)
                 child = Node(new_state, move, current_node)
 
                 # add child by its move
                 current_node.children.append(child)
-                print("number of children: " + str(len(current_node.children)))
+            print("number of children: " + str(len(current_node.children)))
 
-            current_node = self.find_max_node(current_node)
+            current_node = random.choice(current_node.children)
             current_state = copy.deepcopy(current_node.state)
 
-        return current_node, current_state
+        return current_node, current_state, player
 
     def play(self, state, player, move):
         state[move[0]][move[1]] = player
         player = 3 - player
+
+        return state, player
 
     def simulation(self, state, player):
         available_moves = find_empty_cells(state)
@@ -117,10 +116,10 @@ class HexBot:
             move = random.choice(available_moves)
             available_moves.remove(move)
 
-            self.play(state, player, move)
+            state, player = self.play(state, player, move)
 
         print("Game status: " + str(game_status(state)))
-        return game_status(state)
+        return game_status(state), player
 
     def back_propagation(self, node, outcome):
         point = 0
